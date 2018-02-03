5142e130969645bb6d80c2f093f4eef564ed9814
Roee Babayoff <roeebaba@gmail.com>
2018-02-03 13:13:26 +0200

    Maccabistats_web is python package!

---
+++ maccabistats_web/__init__.py
@@ -0,0 +1,3 @@
+from .main import app
+
+version = "0.7.0"
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+import maccabistats_web
+
+setup(
+    name='maccabistats_web',
+    version=maccabistats_web.version,
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='Maccabi tel-aviv football team statistics manipulation web-site',
+    python_requires='>=3',
+    install_requires=["maccabistats==1.2.0",
+                      "flask==0.12.2",
+                      "python-dateutil==2.6.1"]
+)
